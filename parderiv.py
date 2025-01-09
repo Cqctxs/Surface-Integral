f69c4c919d87f58a887daefb447169c08924600a
Alyn Huang <124748265+dogwter@users.noreply.github.com>
2025-01-09 10:18:30 -0500

    paraboloid

---
+++ parderiv.py
@@ -0,0 +1,30 @@
+from manim import *
+
+class partial(ThreeDScene):
+    def construct(self):
+        def f(s,t):
+            return np.array([s,t,0.5*(s*s+t*t)])
+        cut = Surface(
+            lambda u, v: np.array([u, 0, v]),  
+            u_range=[-5, 5],  
+            v_range=[-5, 5], 
+            fill_opacity = 0.2,
+            checkerboard_colors=None 
+        )
+        axes = ThreeDAxes(axis_config={"include_tip": False})
+        para = Surface(
+            lambda u, v: axes.c2p(*f(u, v)),    
+            u_range=[-2, 2],
+            v_range=[-2, 2],
+            fill_color=GREEN,
+            fill_opacity = 1,
+            checkerboard_colors=None 
+        )
+        self.play(Write(axes))
+        self.move_camera(phi=75 * DEGREES, theta=30 * DEGREES, zoom=1, run_time=1.5)
+        self.wait(1)
+        self.play(Write(para, run_time=3))
+        self.wait(1)
+        self.play(Write(para, run_time=1.5),Create(cut, run_time=1.5))
+        self.wait(1)
+        
