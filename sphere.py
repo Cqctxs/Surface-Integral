bfd4c7d7814f270195010c67b06dfe0aaa744933
Sean <seanzhao2007@gmail.com>
2025-01-09 14:08:23 -0500

    sphere animation

--- sphere.py
+++ sphere.py
@@ -3,8 +3,8 @@ from manim import *
 
 class Sphere(ThreeDScene):
     def construct(self):
+        a = 2
         def f(s, t):
-            a = 2
             return np.array([a*np.cos(t)*np.cos(s),a*np.cos(t)*np.sin(s),a*np.sin(t)])
 
         axes = ThreeDAxes(
@@ -16,19 +16,28 @@ class Sphere(ThreeDScene):
             z_length=10
         )
         labels = axes.get_axis_labels(x_label="x", y_label="y", z_label="z")
-        torus = Surface(
+        sphere = Surface(
             lambda u, v: axes.c2p(*f(u, v)),
             u_range=[0, 2 * PI],
             v_range=[-PI/2, PI/2],
-            fill_opacity=0,
+            checkerboard_colors=[BLUE, BLUE],
+            stroke_width=0
         )
 
-        # zoom out so we see the axes
+        # Define the radius line
+        line = Line3D(start=axes.c2p(0, 0, 0), end=axes.c2p(a, 0, 0), color=RED)
+        label = MathTex("a").next_to(line, UP).set_color(RED_A)
+
         self.set_camera_orientation(zoom=0.5)
         self.play(Write(axes), Write(labels))
         self.wait(0.5)
-
-        self.move_camera(phi=75 * DEGREES, theta=30 * DEGREES, zoom=1, run_time=1.5)
-        self.play(Write(torus))
+        self.set_camera_orientation(phi=75 * DEGREES, theta=30 * DEGREES, zoom=1, run_time=1.5)
+        self.play(Write(sphere))
         self.begin_ambient_camera_rotation(rate=0.15)
+        self.wait(5)
+        self.stop_ambient_camera_rotation()
+        self.wait(0.5)
+        self.set_camera_orientation(phi=0 * DEGREES, theta=-90 * DEGREES, zoom=1, run_time=1.5)
+        self.play(AnimationGroup(Write(line), Write(label)))
+        self.bring_to_front(line, label)
         self.wait(5)
