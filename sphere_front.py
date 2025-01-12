a55e5cca062fbdae47b715ba89ca71ece21539be
Alyn Huang <124748265+dogwter@users.noreply.github.com>
2025-01-11 21:09:41 -0500

    Add files via upload

--- sphere_front.py
+++ sphere_front.py
@@ -3,7 +3,7 @@ from manim import *
 
 class Sphere(Scene):
     def construct(self):
-        a = 2
+        a = 4
 
         axes = Axes(
             x_range=[-8, 8],
@@ -16,7 +16,7 @@ class Sphere(Scene):
         y_label = axes.get_y_axis_label("z")
 
         circle = Circle(
-            radius=1,
+            radius=2,
             fill_opacity=0.5,
             color=BLUE
         )
@@ -34,7 +34,7 @@ class Sphere(Scene):
             color=RED
         ))
         
-        angle_label = MathTex("t").next_to(line, RIGHT, buff=0.7).set_color(RED)
+        angle_label = always_redraw(lambda:MathTex("t").next_to(line, RIGHT, buff=0.1).set_color(RED))
         self.camera.frame_height *= 1.35
         self.camera.frame_width *= 1.35
         self.add(axes, x_label, y_label)
@@ -55,9 +55,33 @@ class Sphere(Scene):
         self.play(Rotating(line, radians=PI / 2, about_point=axes.c2p(0, 0)), run_time=2, rate_func=smooth)
         self.wait(1)
 
-        text = MathTex(r"\frac{-\pi}{2} \leq t \leq \frac{\pi}{2}").to_corner(UL)
+        text = MathTex(r"-\frac{\pi}{2} \leq t \leq \frac{\pi}{2}").to_corner(UL)
 
         self.play(Transform(angle_label, text))
         self.remove(angle_label)
         self.add(text)
-        self.wait(5)
+        self.play(Rotating(line, radians=PI / 3, about_point=axes.c2p(0, 0)), run_time=2, rate_func=smooth)
+        
+        end_point = line.get_end()
+        x_val = end_point[0]
+
+        perpendicular_point = axes.c2p(x_val+1, 0)
+
+        perpendicular_line = Line(end_point, perpendicular_point, color=GREEN)
+
+        self.play(Write(perpendicular_line))
+        yline = Line(axes.c2p(0, 0), perpendicular_point, color=GREEN)
+        self.play(Write(yline))
+        self.play(FadeOut(text, axes, circle, x_label, y_label))
+        tr = VGroup(yline, perpendicular_line,line,angle_arc)
+        self.wait(2)
+        # Label "a" under the yline
+        self.play(tr.animate.scale(3))
+        label_a = MathTex("a \cos t").next_to(tr, DOWN, buff=0.3)
+        label_b = MathTex("z = a \sin t").next_to(tr, RIGHT, buff=0.3)
+        self.remove(angle_arc)
+        self.play(Write(label_a))
+        self.play(Write(label_b))
+        self.wait(3)
+
+        
